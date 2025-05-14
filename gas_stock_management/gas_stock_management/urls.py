51f82fcb7e5c1e6e01442d78975a2a53c00dfdf9
Alicelinzy <109695376+Alicelinzy@users.noreply.github.com>
2025-05-14 15:31:22 +0200

    a change in urls

--- gas_stock_management/gas_stock_management/urls.py
+++ gas_stock_management/gas_stock_management/urls.py
@@ -16,13 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-]
-from django.contrib import admin
-from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
